c2d501222d08016755139b8d81e6356ea06b8b4c
Ephemeral25 <clemdem61@gmail.com>
2019-10-23 22:41:05 +0200

    Added Login form UI + Instructions for project install

--- README.md
+++ README.md
@@ -1,29 +1,42 @@
 # webavance2019
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
++++
+
+npm i eslint-config-prettier
+npm i eslint-plugin-prettier
+import 'material-design-icons-iconfont/dist/material-design-icons.css' dans main.js
+npm install --save material-design-icons-iconfont
--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>CAR</span>
+        <span class="font-weight-light">E-shop</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -11,27 +11,28 @@
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">//TODO TWITTER LINK</span>
+
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Login />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Login from './components/Login'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Login
   },
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
